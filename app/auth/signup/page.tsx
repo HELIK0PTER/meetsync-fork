52ad18e3c69330e82050830f7d536547fd7846f5
set2table <pmolik.logan@gmail.com>
2025-03-04 18:52:07 +0100

    page de login et de signup

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,25 +1,32 @@
-
 import Link from "next/link";
 
 export default function SignupPage() {
+
+    const handleClick = () => {
+        // logique de signup
+
+    }
+
     return (
-        <div className="flex items-center justify-center min-h-screen">
+        <div className="flex items-center justify-center min-h-screen]">
             <div className="text-white p-8 rounded-2xl shadow-lg w-96">
                 <h1 className="text-center text-xl font-bold mb-12">MEETSYNC</h1>
-
+                <h2 className="text-center text-3xl mb-8 font-medium">INSCRIVEZ-VOUS</h2>
 
                 <input
                     type="email"
                     placeholder="Email"
-                    className="text-center w-full p-3 mb-4 bg-[#343b4a] rounded-md focus:outline-none text-white placeholder-gray-400"
+                    className="text-center w-full p-3 mb-4 bg-secondary rounded-md focus:outline-none placeholder-gray-400"
                 />
                 <input
                     type="password"
                     placeholder="Mot de passe"
-                    className="text-center w-full p-3 mb-4 bg-[#343b4a] rounded-md focus:outline-none text-white placeholder-gray-400"
+                    className="text-center w-full p-3 mb-4 bg-secondary rounded-md focus:outline-none text-white placeholder-gray-400"
                 />
 
-                <button className="w-full py-3 bg-green-500 text-white font-semibold rounded-md hover:bg-green-600 transition">
+                <button
+                    onClick={handleClick}
+                    className="w-full py-3 bg-green-500 text-white font-semibold rounded-md hover:bg-green-600 transition">
                     Créer le compte
                 </button>
 
@@ -30,9 +37,9 @@ export default function SignupPage() {
                 </button>
 
                 <p className="text-center mt-4 text-sm">
-                    Pas encore de compte ?{" "}
-                    <Link href="/auth/signup" className="text-blue-400 hover:underline">
-                        Inscrivez-vous
+                    Déjà un compte ?{" "}
+                    <Link href="/auth/login" className="text-blue-400 hover:underline">
+                        Connectez-vous
                     </Link>
                 </p>
             </div>
