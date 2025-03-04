ad2a518cf85f0d263a0aa6185d6216ea917c875d
Matheus Kops Guedes <matheuskopsguedes@gmail.com>
2025-03-04 18:10:28 +0100

    correction des tabs

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function loginPage() {
-		return (
-				<div>
-						<h1>Login</h1>
-				</div>
-		)
-}
+const Page = () => {
+	return (
+		<div>
+			test
+		</div>
+	);
+};
+
+export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <div>
-          <header className="flex justify-around text-xl mb-20 mt-3">
-              <h1 className="uppercase">MEETSYNC</h1>
-              <ul className="flex items-center flex-wrap gap-x-2">
-                  <li><a href="#">Produits</a></li>
-                  <li>Plans</li>
-                  <li>Avis</li>
-                  <li>Commencez</li>
-              </ul>
-          </header>
+    <div>
+      <header className="flex justify-around text-xl mb-20 mt-3">
+        <h1 className="uppercase">MEETSYNC</h1>
+        <ul className="flex items-center flex-wrap gap-x-2">
+          <li><a href="#">Produits</a></li>
+          <li>Plans</li>
+          <li>Avis</li>
+          <li>Commencez</li>
+        </ul>
+      </header>
 
-        <div className="flex justify-around text-xl items-center text-center gap-x-2">
-            <div className="w-1/2">
-                <h1 className="text-3xl">Une plateforme de réservation simple, rapide et sécurisée pour vos événements </h1>
-                <h2 className="text-gray-400">Réservez facilement vos évènements en quelques clics, avec une gestion flexible et intuitive.</h2>
-                <h3>Button</h3>
-            </div>
-            <div className="w-1/3 ">
-                <Image src="/maquette.png" alt="Maquettepng logo" width={500} height={500} className=""/>
-            </div>
+      <div className="flex justify-around text-xl items-center text-center gap-x-2">
+        <div className="w-1/2">
+          <h1 className="text-3xl">Une plateforme de réservation simple, rapide et sécurisée pour vos événements </h1>
+          <h2 className="text-gray-400">Réservez facilement vos évènements en quelques clics, avec une gestion flexible et intuitive.</h2>
+          <h3>Button</h3>
+        </div>
+        <div className="w-1/3 ">
+          <Image src="/maquette.png" alt="Maquettepng logo" width={500} height={500} className=""/>
         </div>
       </div>
+    </div>
   );
 }
