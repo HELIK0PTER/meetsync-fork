705da4f231ef9a18ac8e3d18fa8551a54c1c0a6d
SilverHawks <thekeke60@gmail.com>
2025-03-20 16:11:10 +0100

    fix main style and clean code

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,6 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
-import {Divider} from "@heroui/divider";
-import {Spacer} from "@heroui/react";
-import {ThemeSwitch} from "@/components/theme-switch";
 import { UserProvider } from "../lib/UserContext";
 import {Footer} from "@/components/footer";
 
@@ -51,7 +48,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="container flex-grow">
               {children}
             </main>
             <Footer />
